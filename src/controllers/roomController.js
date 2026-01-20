d18ab573ed97f4afb95c3b0d89877329ac473e46
Harshit Bajpai <bajpaih32@gmail.com>
2026-01-20 23:19:47 +0530

    Completed Rooms CRUD with auth, ownership and JWT

--- src/controllers/roomController.js
+++ src/controllers/roomController.js
@@ -27,5 +27,136 @@ const addRoom= async (req,res)=>{
   }
     };
 
+    const getallroom= async (req,res)=> {
+        
+        try {
 
-    module.exports={addRoom};
+            const{city,maxRent}=req.query;
+            let filter={};
+
+             if (city) {
+      filter.city = city;
+    }
+
+    if (maxRent) {
+      filter.rent = { $lte: Number(maxRent) };
+    }
+    const rooms = await Room.find(filter);
+
+    res.status(200).json({
+      count: rooms.length,
+      rooms: rooms,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+    }
+
+    const getsingleroom= async (req,res)=>{
+        try{
+            const {id}=req.params;
+
+            const room = await Room.findById(id);
+             if (!room) {
+      return res.status(404).json({
+        message: "Room not found",
+      });
+    }
+
+    res.status(200).json(room);
+
+        }catch{error}{
+             res.status(400).json({
+      message: "Invalid room id",
+    });
+        };
+    }
+
+    const getmyroom= async (req,res)=>{
+      try{
+
+        const ownerid= req.user.id;
+
+        const rooms= await Room.find({owner:ownerid});
+
+        res.status(200).json({
+      count: rooms.length,
+      rooms,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateroom= async (req,res)=>{
+  try{
+    const {id}= req.params;
+
+    const room =await Room.findById(id);
+
+    if(!room){
+      return res.status(404).json({
+        message: "Room not found",
+      });
+    }
+
+    if (room.owner.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "You are not allowed to update this room",
+      });
+    }
+
+       const updatedRoom = await Room.findByIdAndUpdate(
+      id,
+      req.body,
+      { new: true }
+    );
+
+    res.status(200).json({
+      message: "Room updated successfully",
+      room: updatedRoom,
+    });
+  }catch(error){
+        res.status(400).json({
+      message: error.message,
+    });
+  }
+}
+
+const deleteroom= async (req,res)=>{
+
+  try{
+    const {id}=req.params;
+    const room= await Room.findById(id);
+
+       if (!room) {
+      return res.status(404).json({
+        message: "Room not found",
+      });
+    }
+
+    if (room.owner.toString() !== req.user.id) {
+      return res.status(403).json({
+        message: "You are not allowed to delete this room",
+      });
+    }
+
+    await Room.findByIdAndDelete(id);
+
+    res.status(200).json({
+      message: "Room deleted successfully",
+    });
+
+  }catch(error){
+    res.status(400).json({
+      message: error.message,
+    });
+
+  }
+}
+
+    module.exports={addRoom,getallroom,getsingleroom,getmyroom,updateroom,deleteroom};
--- src/routes/roomRoutes.js
+++ src/routes/roomRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const { addRoom } = require("../controllers/roomController");
+const { addRoom, getallroom,getsingleroom,getmyroom ,updateroom, deleteroom} = require("../controllers/roomController");
 const authMiddleware = require("../middleware/authMiddleware");
 const ownerMiddleware = require("../middleware/ownerMiddleware");
 
@@ -12,5 +12,11 @@ router.post(
   ownerMiddleware,
   addRoom
 );
+
+router.get("/",getallroom);
+router.get("/my",authMiddleware,ownerMiddleware,getmyroom);
+router.get("/:id",getsingleroom);
+router.put("/:id",authMiddleware,ownerMiddleware,updateroom);
+router.delete("/:id",authMiddleware,ownerMiddleware,deleteroom);
 
 module.exports = router;
